9b4e46c16072d91ba2fee1495fabb184a5c3a953
stdioh321 <diaslinoh@gmail.com>
2015-10-10 14:44:41 -0300

    ha
    ha

---
+++ js/js.js
@@ -0,0 +1,10 @@
+	
+$(document).ready(function() {
+      $('.flexslider').flexslider({
+        animation: "slide"
+      });
+
+   //    $( "img:not(.slides img)" ).each(function( index ) {
+	  // });
+	  
+});
