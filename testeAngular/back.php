3621fc8d9a0307c47f44f7dac1efacbf3fb36873
stdioh321 <diaslinoh@gmail.com>
2015-10-26 12:29:04 -0200

    angular
    angular

---
+++ testeAngular/back.php
@@ -0,0 +1,9 @@
+<?php
+		header('Access-Control-Allow-Origin: *');
+		header('Content-Type: text/plain');
+
+		$contatos = "[{nome:'Carlos',id:'1'},{nome:'Maria',id:'2'}]";
+		print_r($contatos);
+
+  
+?>
